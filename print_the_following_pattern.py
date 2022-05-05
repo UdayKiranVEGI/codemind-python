62e6a7651c342e15cd3cb6392c93c472bb365b30
UdayKiranVEGI <84384630+UdayKiranVEGI@users.noreply.github.com>
2022-05-05 17:42:34 +0530

    n=int(input()) a=n m={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'} for i in range(1,n+1):     for k in range(1,a+1):         print(m[a],end=' ')     a-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 n=int(input())
+a=n
+m={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'}
 for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end='')
+    for k in range(1,a+1):
+        print(m[a],end=' ')
+    a-=1
     print()
