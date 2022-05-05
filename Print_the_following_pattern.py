fb4e58274a832317fba0186eaceee084c1ec8228
UdayKiranVEGI <84384630+UdayKiranVEGI@users.noreply.github.com>
2022-05-05 17:11:15 +0530

    n=int(input()) for i in range(1,n+1):     a=i     for j in range(1,n+2-i):         print(a,end=' ')         a+=1     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,8 +1,7 @@
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==j or (j==n+1-i):
-            print(n+1-i,end=' ')
-        else:
-            print(end=' ')
+    a=i
+    for j in range(1,n+2-i):
+        print(a,end=' ')
+        a+=1
     print()
