04b08acfaf0aa73b26edc73c8304e036fc7ae066
UdayKiranVEGI <84384630+UdayKiranVEGI@users.noreply.github.com>
2022-05-06 11:57:35 +0530

    n=int(input()) if n>0:     for i in range(1,n+1):         a=0         print(' '*(n-i),end='')         for k in range(1,2*i):             if k<i:                 print(i-k,end='')             else:                 print(a,end='')                 a+=1         print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,12 @@
 n=int(input())
 if n>0:
     for i in range(1,n+1):
+        a=0
         print(' '*(n-i),end='')
         for k in range(1,2*i):
-            print(i,end='')
+            if k<i:
+                print(i-k,end='')
+            else:
+                print(a,end='')
+                a+=1
         print()
