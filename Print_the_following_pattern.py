caa515450205fdb0d4f1ced7d88f04116e0fb387
UdayKiranVEGI <84384630+UdayKiranVEGI@users.noreply.github.com>
2022-05-06 12:04:19 +0530

    n=int(input()) d={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'} if n>0:     for i in range(1,n+1):         a=0         print(' '*(n-i),end='')         j=2         for k in range(1,2*i):             if k<=i:                 print(d[k],end='')             else:                 print(d[k-j],end='')                 j+=2         print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,7 +1,14 @@
 n=int(input())
-for i in range(1,n+1):
-    a=i
-    for j in range(1,n+2-i):
-        print(a,end=' ')
-        a+=1
-    print()
+d={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'}
+if n>0:
+    for i in range(1,n+1):
+        a=0
+        print(' '*(n-i),end='')
+        j=2
+        for k in range(1,2*i):
+            if k<=i:
+                print(d[k],end='')
+            else:
+                print(d[k-j],end='')
+                j+=2
+        print()
