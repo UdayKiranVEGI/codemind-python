ae0cf8c492a4c18c8a1053eab41e3e9ed37b9dd6
UdayKiranVEGI <84384630+UdayKiranVEGI@users.noreply.github.com>
2022-05-06 12:05:48 +0530

    n=int(input()) if n>0:     for i in range(1,n+1):         a=0         print(' '*(n-i),end='')         j=2         for k in range(1,2*i):             if k<=i:                 print(k,end='')             else:                 print(k-j,end='')                 j+=2         print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,13 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(i,end=' ')
-    print()
+if n>0:
+    for i in range(1,n+1):
+        a=0
+        print(' '*(n-i),end='')
+        j=2
+        for k in range(1,2*i):
+            if k<=i:
+                print(k,end='')
+            else:
+                print(k-j,end='')
+                j+=2
+        print()
